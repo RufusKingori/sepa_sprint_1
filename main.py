925292bb745e86fde0eb10d15a1ff2e3697151bd
Rufus Kingori <kingsb2709@gmail.com>
2021-11-02 18:55:53 +0300

    queries on customer and product details

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ product_list = [{'id': '4056', 'name': 'fries', 'Quantity': '10', 'price': '200'
                 {'id': '2589', 'name': 'buns', 'Quantity': '20', 'price': '120'},
                 {'id': '7860', 'name': 'smokies', 'Quantity': '32', 'price': '50'},
                 {'id': '7410', 'name': 'soda', 'Quantity': '55', 'price': '60'}]
-purchase_list = [{"c_name":"rufus","p_name":"buns","quantity":"5","price":"120"}]
+
 
 def main():
     print("*****Welcome to Kings foods and beverages*****")
@@ -106,7 +106,7 @@ def queries():
                            "[1] Search a product\n"
                            "[2] List all customers\n"
                            "[3] List all products\n"
-                           "[4] List a customer\n"
+                           "[4] List a customer's name, products bought and total spent\n"
                            "[5] Quit\n"))
     if queries_op == 1:
         search_product()
@@ -115,7 +115,7 @@ def queries():
     elif queries_op == 3:
         print(product_list)
     elif queries_op == 4:
-        list_customer()
+        purchase_data()
     elif queries_op == 5:
         quit()
     else:
@@ -262,16 +262,28 @@ def list_customer():
 def purchase_data():
     #This program writes to file the customer's name, products purchased, quantity purchased
     #and the total amout to be paid.
-    purchase_amount = []
+    product_details = []
     total = 0
     id_customer = input("Enter the customer id:")
-    for id_customer in range(len(purchase_list)):
-        c_id = purchase_list[i]["id"]
-        if c_id == id_customer:
-            customer_name = purchase_list[i]['c_name']
+    for i in range(len(purchase_list)):
+        c_id = purchase_list[i]["c_id"]
+        if id_customer in c_id:
+            c_name = purchase_list[i]['c_name']
+            p_name = purchase_list[i]['p_name']
+            quantity = purchase_list[i]['quantity']
+            price = purchase_list[i]['price']
+            total_price = int(price) * int(quantity)
+            product_d = {"p_name":p_name,"quantity":quantity}
+            product_details.append(product_d)
+            total += total_price
+    print(c_name,"has bought",product_details,"for",total)
 
 
-
+#Pre-existing purchase list with details of customer and product purchased.
+purchase_list = [{"c_id":"2709","c_name":"rufus","p_name":"buns","quantity":"3","price":"120"},
+                 {"c_id":"1439","c_name":"faith","p_name":"soda","quantity":"2","price":"60"},
+                 {"c_id":"3520","c_name":"amon","p_name":"fries","quantity":"2","price":"200"},
+                 {"c_id":"1439","c_name":"faith","p_name":"smokies","quantity":"5","price":"50"}]
 main()
 
 
